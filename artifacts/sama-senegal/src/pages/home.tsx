81ac5ba4db139a86fb9b83fd308981da85e9af95
Karimaw140906 <awrimka14@gmail.com>
2026-05-21 02:49:20 +0000

    erreur

--- artifacts/sama-senegal/src/pages/home.tsx
+++ artifacts/sama-senegal/src/pages/home.tsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import { Navbar } from "@/components/Navbar";
+import React from 'react';
 import { Hero } from "@/components/Hero";
 import { Stats } from "@/components/Stats";
 import { Tours } from "@/components/Tours";
